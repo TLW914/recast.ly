b6b54d66e59c4727b526e889f82ad558bf0d5d0d
Tyler Watson <leggettwatson.tyler@gmail.com>
2018-11-27 13:54:31 -0800

    Added functionality for videoplayer, videolistview, and click events. updated youtube api key

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -3,8 +3,8 @@ import VideoListEntry from '../components/VideoListEntry.js';
 var VideoList = (props) => {
   return(
     <div className="video-list">
-    {props.videos.map(video =>
-      <VideoListEntry video={video}/>
+    {props.videos.map((video) =>
+      <VideoListEntry video={video} clicked={props.click} />
     )}
   </div>
   )};
@@ -19,3 +19,4 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
+
--- src/config/youtube.example.js
+++ src/config/youtube.example.js
@@ -1,4 +1,4 @@
 // Put your YouTube API keys here!
-var YOUTUBE_API_KEY = 'YOUR_API_KEY_HERE';
+var YOUTUBE_API_KEY = 'AIzaSyAAwKsVILQqu-1Jb1V6PVEoSZUwjadoJLk';
 
 export default YOUTUBE_API_KEY;
